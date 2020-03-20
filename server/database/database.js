907333aa307c5002674a90bb8bc96163b7e5f328
Chandler King <ckmotox356@gmail.com>
2020-03-19 19:35:36 -0600

    Changed an env variable.

--- server/database/database.js
+++ server/database/database.js
@@ -1,6 +1,6 @@
 const dbConfig = {
     connectionLimit : 10,
-    host : process.env.DB_HOST,
+    host : process.env.DB_HOST_NAME,
     user : process.env.DB_USERNAME,
     password : process.env.DB_PASSWORD,
     database : process.env.DB_NAME,
