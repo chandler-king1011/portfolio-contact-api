f423ce7a177d3c943bdace562302f159bc1e4f83
Chandler King <ckmotox356@gmail.com>
2020-03-20 19:50:01 -0600

    Fixed cors problem from local host.

--- server/app.js
+++ server/app.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const cors = require("cors");
 require('dotenv').config();
 const mysql = require('mysql');
 
@@ -12,7 +11,7 @@ const dbConn = mysql.createPool(dbConfig);
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+
 app.use(express.json());
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
